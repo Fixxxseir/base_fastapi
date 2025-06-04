562dfa5b274d8503cec17181f71f1b00d79754fe
mexic <mexic2284@mail.ru>
2025-06-05 00:14:21 +0400

    create user manager

---
+++ fastapi_app/core/authentication/user_manager.py
@@ -0,0 +1,27 @@
+import logging
+from typing import Optional, TYPE_CHECKING
+
+from fastapi_users import BaseUserManager, IntegerIDMixin
+
+from core.models import User
+from core.config import settings
+from core.types.user_id import UserIDType
+
+if TYPE_CHECKING:
+    from fastapi import Request
+
+log = logging.getLogger(__name__)
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, UserIDType]):
+    reset_password_token_secret = (
+        settings.access_token_secret.reset_password_token_secret
+    )
+    verification_token_secret = settings.access_token_secret.verification_token_secret
+
+    async def on_after_register(
+        self,
+        user: User,
+        request: Optional["Request"] = None,
+    ):
+        log.warning("User %r has registered.", user.id)
