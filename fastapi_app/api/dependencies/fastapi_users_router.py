b93b53d36f484a57b5b3431456a0c2281c085351
mexic <mexic2284@mail.ru>
2025-06-05 00:37:49 +0400

    create fastapi users routers helper

---
+++ fastapi_app/api/dependencies/fastapi_users_router.py
@@ -0,0 +1,13 @@
+import uuid
+
+from fastapi_users import FastAPIUsers
+
+from core.models.user import User
+from core.types.user_id import UserIDType
+from .user_manager import get_user_manager
+from .backend import authentication_backend
+
+fastapi_users = FastAPIUsers[User, UserIDType](
+    get_user_manager,
+    [authentication_backend],
+)
