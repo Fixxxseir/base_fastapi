9d61deace81e1c059d22150faf4eddb7bcaec972
mexic <mexic2284@mail.ru>
2025-06-04 22:50:58 +0400

    create users and access_tokens dbs dependencies

---
+++ fastapi_app/api/dependencies/users.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+from fastapi import Depends
+from sqlalchemy.sql.annotation import Annotated
+
+from core.models import db_helper, User
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def get_user_db(
+    session: Annotated[
+        "AsyncSession",
+        Depends(db_helper.session_getter),
+    ],
+):
+    yield User.get_db(session=session)
