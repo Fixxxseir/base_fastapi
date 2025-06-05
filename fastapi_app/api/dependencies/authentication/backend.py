2a18990aff8cdedf69f82f5493e0efa2f7f90798
mexic <mexic2284@mail.ru>
2025-06-06 01:03:13 +0400

    rename: authentication_backend -> auth_backend

--- fastapi_app/api/dependencies/authentication/backend.py
+++ fastapi_app/api/dependencies/authentication/backend.py
@@ -6,7 +6,7 @@ from .strategy import get_database_strategy
 from core.authentication.transport import bearer_transport
 
 
-authentication_backend = AuthenticationBackend(
+auth_backend = AuthenticationBackend(
     name="access-tokens-db",
     transport=bearer_transport,
     get_strategy=get_database_strategy,
